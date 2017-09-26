4aa6c129623994a4b28c464709ea687a695e6098
BaggioPereira <baggio_111111@hotmail.com>
2017-09-26 15:07:01 +0100

    Demo creating primitives and setting parents

---
+++ Assets/Scripts/CreatureBuild.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureBuild : MonoBehaviour {
+    public int NumberOfHeads = 0;
+    public int NumberOfBodyParts = 0;
+    public int NumberOfArms = 0;
+    public int NumberOfLowerBody = 0;
+    public int NumberOfLegs = 0;
+    private ArrayList Nodes;
+    GameObject Creature, CreatureParts;
+	// Use this for initialization
+	void Start ()
+    {
+        Creature = new GameObject("Creature");
+        Nodes = new ArrayList();
+        CreateLowerBody();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void CreateHead()
+    {
+
+    }
+
+    void CreateBody()
+    {
+
+    }
+
+    void CreateArms()
+    {
+
+    }
+
+    void CreateLowerBody()
+    {
+        for(int i = 0; i < NumberOfLowerBody; i++)
+        {
+            CreatureParts = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            CreatureParts.transform.position = new Vector3(i * 5, 0, 0);
+            CreatureParts.transform.parent = Creature.transform;
+            Nodes.Add(CreatureParts);
+        }
+    }
+
+    void CreateLegs()
+    {
+
+    }
+}
